ccc8e9d2e5d6ffe6688ac6f5b0a0f45a65fd513c
byeunghoon Kang <qudgns119@gmail.com>
2024-03-25 12:14:18 +0900

    test

---
+++ src/test/java/backend/time/TimeApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.time;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
